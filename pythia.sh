bd873d9c2279ebdd9bf966ffb7dc43bdec1c4d37
Tim Lawson <tim.lawson@bristol.ac.uk>
2025-03-26 06:43:48 +0000

    add pythia embed_in dataset

---
+++ pythia.sh
@@ -0,0 +1,19 @@
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 128
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 128 --ffn
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 128 --gaussian
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 128 --ffn --gaussian
+
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 256
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 256 --ffn
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 256 --gaussian
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 256 --ffn --gaussian
+
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 512
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 512 --ffn
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 512 --gaussian
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 512 --ffn --gaussian
+
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 1024
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 1024 --ffn
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 1024 --gaussian
+python experiments/pythia.py --model_name EleutherAI/pythia-14m --num_latents 1024 --ffn --gaussian
